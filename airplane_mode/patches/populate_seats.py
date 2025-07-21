8b40163b95e99614e8c5d901643f413f3b7a49af
muthanii <muthanalsaadi@gmail.com>
2025-07-21 17:32:48 +0000

    feat: create a patch.

---
+++ airplane_mode/patches/populate_seats.py
@@ -0,0 +1,13 @@
+import random
+import frappe
+
+
+def execute():
+	tickets = frappe.get_all("Airplane Ticket", filters={"seat": ["is", "set"]}, fields=["name"])
+	print(f"Found {len(tickets)} tickets to update")
+
+	for ticket in tickets:
+		seat = f"{random.randint(1, 99)}{random.choice(['A', 'B', 'C', 'D', 'E'])}"
+		frappe.db.set_value("Airplane Ticket", ticket.name, "seat", seat)
+
+	print("Seats updated successfully.")
