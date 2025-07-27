0a2b649d9ad42369f0e3d9b586b0957bc803c96a
muthanii <muthanalsaadi@gmail.com>
2025-07-27 10:11:17 +0000

    feat: added type checking for the airplane python file.

--- airplane_mode/airplane_mode/doctype/airplane/airplane.py
+++ airplane_mode/airplane_mode/doctype/airplane/airplane.py
@@ -6,4 +6,17 @@ from frappe.model.document import Document
 
 
 class Airplane(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		airline: DF.Link
+		capacity: DF.Int
+		model: DF.Data
+	# end: auto-generated types
+
 	pass
