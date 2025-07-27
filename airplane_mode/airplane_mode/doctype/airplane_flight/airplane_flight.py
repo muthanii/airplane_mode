27596e8452cf65c8aec14afb5f2d79533944a9c2
muthanii <muthanalsaadi@gmail.com>
2025-07-27 10:12:15 +0000

    feat: adding the enqueue for the default gate number.

--- airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.py
+++ airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.py
@@ -2,19 +2,27 @@
 # For license information, please see license.txt
 
 import frappe
+from frappe import enqueue
 from frappe.website.website_generator import WebsiteGenerator
+
+from airplane_mode.utils.jobs import update_ticket_gates_in_background
 
 
 class AirplaneFlight(WebsiteGenerator):
 	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
 	from typing import TYPE_CHECKING
 
 	if TYPE_CHECKING:
+		from airplane_mode.airplane_mode.doctype.flight_crew_member.flight_crew_member import FlightCrewMember
 		from frappe.types import DF
 
 		airplane: DF.Link
 		amended_from: DF.Link | None
+		crew_members: DF.Table[FlightCrewMember]
 		date_of_departure: DF.Date
+		default_gate_number: DF.Data | None
 		destination_airport: DF.Link
 		destination_airport_code: DF.Data | None
 		duration: DF.Duration
@@ -28,3 +36,14 @@ class AirplaneFlight(WebsiteGenerator):
 
 	def on_submit(self):
 		self.status = "Completed"
+
+	def on_update(self):
+		if self.has_value_changed("default_gate_number"):
+			enqueue(
+				update_ticket_gates_in_background,
+				queue="default",
+				flight_name=self.name,
+				new_gate=self.default_gate_number,
+				timeout=300,
+			)
+			frappe.msgprint("Ticket update job has been queued.")
